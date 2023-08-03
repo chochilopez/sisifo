3935968127e27a58427d2577f1012761fc024bd9
Gastón López <lopezwgaston@gmail.com>
2023-08-03 08:33:51 -0300

    Initial commit for Sísifo

---
+++ src/main/java/muni/eolida/sisifo/SisifoApplication.java
@@ -0,0 +1,13 @@
+package muni.eolida.sisifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisifoApplication.class, args);
+	}
+
+}
